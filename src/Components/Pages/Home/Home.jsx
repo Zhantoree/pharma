e68fa71cbf8bbf5d8d77e666d03a02c5411e899a
unknown <drug2334@gmail.com>
2023-06-21 06:00:35 +0600

    header fully ready and homepage banner also

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
 import Header from "../../Utils/Header/Header.jsx";
 import s from "./Home.module.css"
-import homeImg from '../../../assets/Images/Homeimg.png'
+import Button from "../../Utils/Buttons/Button.jsx";
 
 const Home = () => {
     return (
         <div className={s.home}>
             <Header/>
-            <div className="container">
-                <div className={s.home_item}>
-                    <img src={homeImg} alt="homeImg"/>
+            <div className={s.home_item}>
+                <div className={s.banner}>
+                    <div className={`${s.container} ${s.banner_inner}`}>
+
+                        <p className={s.banner_suptitle}>
+                            MEDICAL PROFESSIONALS
+                        </p>
+                        <p className={s.banner_title}>
+                            Makes Your Health Better Will Makes Us Better
+                        </p>
+                        <p className={s.banner_subtitle}>
+                            Our team of highly trained professionals uses the latest healing technologies to restore you
+                            to pain-free health quickly and easily.
+                        </p>
+
+                        <Button text={"Get Started"}/>
+                    </div>
                 </div>
             </div>
 
--- src/Components/Utils/Header/Header.jsx
+++ src/Components/Utils/Header/Header.jsx
@@ -1,48 +1,57 @@
 import s from "./Header.module.css";
-import Email from '../../../assets/Icons/Email.png'
-import Phone from '../../../assets/Icons/Phone.png'
+import Email from '../../../assets/Icons/Email.svg'
+import Phone from '../../../assets/Icons/Phone.svg'
 import Logo from '../../../assets/Icons/Logo.png'
 import {NavLink} from "react-router-dom";
 import Button from "../Buttons/Button.jsx";
 
 const Header = () => {
     return (
-        <div className={s.header}>
-            <div className="container">
+        <>
+            <div className={s.header}>
                 <div className={s.header_item}>
-                    <p>Welcome to Pharma. We provides medical accessories</p>
-                    <div className={s.number}>
-                        {/* eslint-disable-next-line no-undef */}
-                        <img src={Phone} alt=""/>
-                        <p>(404) 850 - 7080</p>
-                    </div>
-                    <div className={s.number}>
-                        <img src={Email} alt=""/>
-                        <p>(404) 850 - 7080</p>
-                    </div>
-                </div>
+                    <div className={s.container}>
+                        <div className={s.number}>
+                            <div className={s.number_inner}>
+                                <p className={s.number_text}>Welcome to Pharma. We provides medical accessories</p>
 
-                <div className={s.header_item}>
-                    <NavLink to={"/"}>
-                        <div className={s.logo}>
-                            <img src={Logo} alt="logo"/>
-                            <span>Pharma</span>
+                            </div>
+                            <div className={s.number_inner}>
+                                {/* eslint-disable-next-line no-undef */}
+                                <img className={s.number_inner_img} src={Phone} alt=""/>
+                                <p className={s.number_text}>(404) 850 - 7080</p>
+                            </div>
+                            <div className={s.number_inner}>
+                                <img className={s.number_inner_img} src={Email} alt=""/>
+                                <p className={s.number_text}>(404) 850 - 7080</p>
+                            </div>
                         </div>
-                    </NavLink>
-                    <div className={s.navbar}>
-                        <NavLink to={"/"}>Home</NavLink>
-                        <NavLink to={"/about"}>About</NavLink>
-                        <NavLink to={"/service"}>Service</NavLink>
-                        <NavLink to={"/pages"}>Pages</NavLink>
-                        <NavLink to={"/contact"}>Contact us</NavLink>
                     </div>
-                    <NavLink to={"/contact"}>
-                        <Button text={"Appointment"}/>
-                    </NavLink>
                 </div>
-            </div>
 
-        </div>
+                <div className={s.container}>
+                    <div className={s.navbar}>
+                        <NavLink to={"/"}>
+                            <div className={s.logo}>
+                                <img className={s.logo_img} src={Logo} alt="logo"/>
+                                <span>Pharma</span>
+                            </div>
+                        </NavLink>
+                        <div className={s.navbar_item}>
+                            <NavLink className={s.navbar_item_link} to={"/"}>Home</NavLink>
+                            <NavLink className={s.navbar_item_link} to={"/about"}>About</NavLink>
+                            <NavLink className={s.navbar_item_link} to={"/service"}>Service</NavLink>
+                            <NavLink className={s.navbar_item_link} to={"/pages"}>Pages</NavLink>
+                            <NavLink className={s.navbar_item_link} to={"/contact"}>Contact us</NavLink>
+                        </div>
+                        <NavLink to={"/contact"}>
+                            <Button text={"Appointment"}/>
+                        </NavLink>
+                    </div>
+                </div>
+
+            </div>
+        </>
     );
 };
 
